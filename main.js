9aa7e164ee15cf2a5c1eec835283c8c170cde6d2
coderrudrakshgupta <76689919+coderrudrakshgupta@users.noreply.github.com>
2021-06-20 13:18:11 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 
-  Webcam.set({
+
+
+Webcam.set({
     width:350,
     height:300,
     image_format : 'png',
     png_quality:90
   });
 
-  camera = document.getElementById("camera");
+camera = document.getElementById("camera");
 
-Webcam.attach( '#camera' );
+Webcam.attach('#camera');
 
       
 function take_snapshot()
@@ -20,58 +22,28 @@ function take_snapshot()
 
   console.log('ml5 version:', ml5.version);
   
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/xQRttb4CE/model.json',modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/model.json',modelLoaded);
 
   function modelLoaded() {
     console.log('Model Loaded!');
   }
-      
+  
+
+
   function check()
   {
     img = document.getElementById('captured_image');
     classifier.classify(img, gotResult);
   }
 
+
 function gotResult(error, results) {
   if (error) {
     console.error(error);
   } else {
     console.log(results);
-    
     document.getElementById("result_object_name").innerHTML = results[0].label;
-
-    gesture = results[0].label;
+    document.getElementById("result_object_accuracy").innerHTML = results[1].confidence.toFixed(3);
     
-    toSpeak = "";
-    
-    if(gesture == "amazing")
-    {
-      toSpeak = "This is looking amazing";
-      document.getElementById("result_object_gesture_icon").innerHTML = "&#128076;";
-    }
-    else if(gesture == "best")
-    {
-      toSpeak = "All the best";
-      document.getElementById("result_object_gesture_icon").innerHTML = "&#128077;";
-    }
-    else if(gesture == "victory")
-    {
-      toSpeak = "That was the marvelous victory";
-      document.getElementById("result_object_gesture_icon").innerHTML = "&#9996;";
-    }
-
-    speak();
   }
-}
-
-
-function speak(){
-    var synth = window.speechSynthesis;
-
-    speak_data = toSpeak;
-
-    var utterThis = new SpeechSynthesisUtterance(speak_data);
-
-    synth.speak(utterThis);
-
 }
